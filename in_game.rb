8ec4275d7c2dd571fc72b793b43a418fae259b9c
Corin Schedler <corin@dubdromic.com>
2016-06-30 11:13:56 -0500

    Add background image

--- in_game.rb
+++ in_game.rb
@@ -2,6 +2,7 @@ class InGame < State
   def initialize(screen)
     @player = Player.new screen
     @bullets = []
+    @background = Gosu::Image.new('media/background.png', tileable: true)
     super screen
   end
 
@@ -14,11 +15,12 @@ class InGame < State
   end
 
   def draw
+    background.draw_rot(background.height/2, background.width/2, 0, 90)
     player.draw
     bullets.each(&:draw)
   end
 
   private
 
-  attr_reader :player, :bullets
+  attr_reader :player, :bullets, :background
 end
--- main_menu.rb
+++ main_menu.rb
@@ -2,6 +2,7 @@ class MainMenu < State
   def initialize(screen)
     @title_font = Gosu::Font.new 40
     @description_font = Gosu::Font.new 20
+    @background = Gosu::Image.new('media/background.png', tileable: true)
     super screen
   end
 
@@ -11,11 +12,12 @@ class MainMenu < State
   end
 
   def draw
+    background.draw_rot(background.height/2, background.width/2, 0, 90)
     title_font.draw("Legatus", 150, 200, 1)
     description_font.draw('Press Spacebar', 150, 255, 1)
   end
 
   private
 
-  attr_reader :title_font, :description_font
+  attr_reader :title_font, :description_font, :background
 end
