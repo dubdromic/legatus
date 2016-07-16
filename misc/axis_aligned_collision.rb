a2dd90ec17c3bbe8d8988495609d293dc5641d83
Corin Schedler <corin@dubdromic.com>
2016-07-16 16:22:52 -0500

    Fix axis aligned collision math

--- misc/axis_aligned_collision.rb
+++ misc/axis_aligned_collision.rb
@@ -1,6 +1,8 @@
 class AxisAlignedCollision
   def self.check(one, two)
-    (one.x - two.x).abs * 2 < (one.w + two.w) &&
-      (one.y - two.y).abs * 2 < (one.h + two.h)
+    one.x < two.x + two.w &&
+      one.x + one.w > two.x &&
+      one.y < two.y + two.h &&
+      one.h + one.y > two.y
   end
 end
