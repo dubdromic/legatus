10ffbdeda110cbed1c376986b687f06d3861adb3
Chris Schedler <schedler.chris@gmail.com>
2016-07-17 17:33:13 -0500

    Add the stellar background movement

--- states/in_game.rb
+++ states/in_game.rb
@@ -1,8 +1,22 @@
 class InGame < State
+  BACKGROUND_X = BackgroundImage.image.height / 2
+
   def initialize(screen)
     @entity_pool = EntityPool.new
     @last_enemy_time = Gosu::milliseconds
     entity_pool.add Player.new(screen)
+
+    @backgroundDelta = 1
+
+    @backgroundY_start = background_image.height / 2
+    @backgroundY_end = screen.h * 1.5
+
+    @background1 = background_image 
+    @background2 = background_image
+
+    @background1Y = @backgroundY_start
+    @background2Y = -(@backgroundY_start * 2)
+
     super screen
   end
 
@@ -10,18 +24,33 @@ class InGame < State
     add_new_enemy if spawn_enemy?
     entity_pool.update input
     end_game if player_killed?
-
     self
   end
 
   def draw
-    background_image.draw_rot(background_image.height / 2, background_image.width / 2, 0, 90)
+    move_background
     entity_pool.draw
   end
 
   private
 
-  attr_reader :entity_pool, :background, :last_enemy_time
+  attr_reader :entity_pool, :background, :last_enemy_time,:backgroundY_start, :backgroundY_end, :backgroundDelta
+  attr_accessor :background1, :background2
+
+  def move_background
+    @background1Y += @backgroundDelta
+    @background2Y += @backgroundDelta
+
+    @background1Y = background_image_reset?(@background1Y) ? -(@backgroundY_start * 2) : @background1Y + @backgroundDelta
+    @background2Y = background_image_reset?(@background2Y) ? -(@backgroundY_start * 2) : @background2Y + @backgroundDelta
+
+    @background1.draw_rot(BACKGROUND_X, @background1Y, 0, 90)
+    @background2.draw_rot(BACKGROUND_X, @background2Y, 0, 90)
+  end
+
+  def background_image_reset?(y)
+    y > @backgroundY_end
+  end
 
   def end_game
     @next = GameOver.start screen
